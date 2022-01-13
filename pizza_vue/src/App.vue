b1372334f531de6f3b5769b4ddeb54f895d82214
IrePro78 <gos3r78@gmail.com>
2022-01-13 12:38:30 +0100

    start_new_project

--- pizza_vue/src/App.vue
+++ pizza_vue/src/App.vue
@@ -1,42 +1,45 @@
-<!--<template>-->
-<!--  <div id="nav">-->
-<!--    <router-link to="/">Home</router-link> |-->
-<!--    <router-link to="/about">About</router-link>-->
-<!--  </div>-->
-<!--  <router-view/>-->
-<!--</template>-->
-
 <template>
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>Pizza Ordering</strong></router-link>
+        <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+           @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <div class="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
         <div class="navbar-end">
-          <template v-if="false">
-            <router-link to="/menu" class="navbar-item">Menu</router-link>
-            <router-link to="/sale" class="navbar-item">Sale</router-link>
-            <router-link to="/delivery" class="navbar-item">Delivery</router-link>
-            <router-link to="/orders/add" class="navbar-item">Order</router-link>
-            <router-link to="/my-account" class="navbar-item">My account</router-link>
+          <router-link to="/menu" class="navbar-item">Menu</router-link>
+          <router-link to="/sale" class="navbar-item">Sale</router-link>
 
-          </template>
+          <div class="navbar-item">
+            <div class="buttons">
+              <template v-if="true">
+                <router-link to="/my-account" class="button is-light">My account</router-link>
+              </template>
 
-          <template v-else>
-            <router-link to="/" class="navbar-item">Home</router-link>
-
-            <div class="navbar-item">
-              <div class="buttons">
-                <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+              <template v-else>
                 <router-link to="/log-in" class="button is-light">Log in</router-link>
-              </div>
+              </template>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart ({{ cartTotalLength }})</span>
+              </router-link>
             </div>
-          </template>
+          </div>
         </div>
       </div>
     </nav>
+
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
+      <div class="lds-dual-ring"></div>
+    </div>
 
     <section class="section">
       <router-view/>
@@ -49,25 +52,79 @@
 </template>
 
 <script>
-  import axios from 'axios'
-  export default {
-    name: 'App',
-    beforeCreate() {
-      this.$store.commit('initializeStore')
-      const token = this.$store.state.token
-      if (token) {
-        axios.defaults.headers.common['Authorization'] = "Token " + token
-      } else {
-        axios.defaults.headers.common['Authorization'] = ""
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+      cart: {
+        items: []
       }
     }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+    const token = this.$store.state.token
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
+    }
+  },
+  mounted() {
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity
+      }
+      return totalLength
+    }
   }
+}
 </script>
 
-
-
-
-
 <style lang="scss">
-@import "../node_modules/bulma";
+@import '../node_modules/bulma';
+
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+
+  &.is-loading {
+    height: 80px;
+  }
+}
 </style>
--- pizza_vue/src/views/LogIn.vue
+++ pizza_vue/src/views/LogIn.vue
@@ -1,96 +1,88 @@
 <template>
-  <div class="container mt-5">
-    <div class="columns">
-      <div class="column is-4 is-offset-4">
-        <h1 class="title">Log in</h1>
+    <div class="page-log-in">
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title">Log in</h1>
 
-        <form @submit.prevent="submitForm">
-          <div class="field">
-            <label>Name</label>
-            <div class="control">
-              <input type="text" name="username" class="input" v-model="username">
+                <form @submit.prevent="submitForm">
+                    <div class="field">
+                        <label>Username</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="username">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Password</label>
+                        <div class="control">
+                            <input type="***REMOVED***" class="input" v-model="***REMOVED***">
+                        </div>
+                    </div>
+
+                    <div class="notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                    </div>
+
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-dark">Log in</button>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    Or <router-link to="/sign-up">click here</router-link> to sign up!
+                </form>
             </div>
-          </div>
-
-          <div class="field">
-            <label>Password</label>
-            <div class="control">
-              <input type="***REMOVED***" name="***REMOVED***" class="input" v-model="***REMOVED***">
-            </div>
-          </div>
-
-          <div class="notification is-danger" v-if="errors.length">
-            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
-          </div>
-
-          <div class="field">
-            <div class="control">
-              <button class="button is-success">Submit</button>
-            </div>
-          </div>
-        </form>
-      </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-  name: 'LogIn',
-  data() {
-    return {
-      username: '',
-      ***REMOVED***: '',
-      errors: []
-    }
-  },
-  methods: {
-    async submitForm() {
-      this.$store.commit('setIsLoading', true)
-      axios.defaults.headers.common['Authorization'] = ''
-      localStorage.removeItem('token')
-      const formData = {
-        username: this.username,
-        ***REMOVED***: this.***REMOVED***
-      }
-      await axios
-          .post('/api/v1/auth/token/login/', formData)
-          .then(response => {
-            const token = response.data.auth_token
-            this.$store.commit('setToken', token)
-            axios.defaults.headers.common['Authorization'] = 'Token ' + token
-            localStorage.setItem('token', token)
-          })
-          .catch(error => {
-            if (error.response) {
-              for (const property in error.response.data) {
-                this.errors.push(`${property}: ${error.response.data[property]}`)
-              }
-            } else if (error.message) {
-              this.errors.push('Something went wrong. Please try again!')
+    name: 'LogIn',
+    data() {
+        return {
+            username: '',
+            ***REMOVED***: '',
+            errors: []
+        }
+    },
+    mounted() {
+        document.title = 'Log In | Djackets'
+    },
+    methods: {
+        async submitForm() {
+            axios.defaults.headers.common["Authorization"] = ""
+            localStorage.removeItem("token")
+            const formData = {
+                username: this.username,
+                ***REMOVED***: this.***REMOVED***
             }
-          })
-      await axios
-          .get('/api/v1/users/me/')
-          .then(response => {
-            this.$store.commit('setUser', {
-              'userid': response.data.id,
-              'username': response.data.username,
-              'email': response.data.email,
-              'date_joined': response.data.date_joined,
-            })
-            localStorage.setItem('username', response.data.username)
-            localStorage.setItem('userid', response.data.id)
-            localStorage.setItem('email', response.data.email)
-            localStorage.setItem('date_joined', response.data.date_joined)
-            this.$router.push('/my-account')
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      this.$store.commit('setIsLoading', false)
+            await axios
+                .post("/api/v1/token/login/", formData)
+                .then(response => {
+                    const token = response.data.auth_token
+                    this.$store.commit('setToken', token)
+
+                    axios.defaults.headers.common["Authorization"] = "Token " + token
+                    localStorage.setItem("token", token)
+                    const toPath = this.$route.query.to || '/cart'
+                    this.$router.push(toPath)
+                })
+                .catch(error => {
+                    if (error.response) {
+                        for (const property in error.response.data) {
+                            this.errors.push(`${property}: ${error.response.data[property]}`)
+                        }
+                    } else {
+                        this.errors.push('Something went wrong. Please try again')
+
+                        console.log(JSON.stringify(error))
+                    }
+                })
+        }
     }
-  }
 }
 </script>
