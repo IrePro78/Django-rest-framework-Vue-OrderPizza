df4916e5d691ceddfacea36c746fa711921dfcb3
IrePro78 <gos3r78@gmail.com>
2022-01-21 14:56:03 +0100

    configure_views_products

--- pizza_django/product/urls.py
+++ pizza_django/product/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from product import views
 
 urlpatterns = [
-    path('latest-products/', views.LatestProductsList.as_view()),
+    path('popular-products/', views.PopularProductsList.as_view()),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
     path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
 ]
