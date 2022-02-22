7f7f58496975f144fb2b110475e4db384e4fdc29
IrePro78 <gos3r78@gmail.com>
2022-02-22 15:51:21 +0100

    add_choice_toppings

--- pizza_django/order/urls.py
+++ pizza_django/order/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from order import views
+from order.views import CheckoutViewSet, OrdersListViewSet
+
+router = DefaultRouter()
+
+router.register("checkout", CheckoutViewSet, basename="order")
+router.register("orders", OrdersListViewSet, basename="order")
 
 urlpatterns = [
-    path('checkout/', views.checkout),
-    path('orders/', views.OrdersList.as_view()),
+    path('', include(router.urls)),
+
 ]
