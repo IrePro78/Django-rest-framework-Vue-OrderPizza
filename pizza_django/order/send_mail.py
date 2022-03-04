31ac8c644e96698af18e711de02c95420ad591ea
IrePro78 <78473540+IrePro78@users.noreply.github.com>
2022-03-04 19:24:51 +0100

    add_send_mail

--- pizza_django/order/send_mail.py
+++ pizza_django/order/send_mail.py
@@ -2,8 +2,8 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-username = 'flaskdjangopython@gmail.com'
-password = 'Ciap3k123#'
+username = ''
+password = ''
 
 
 def send_mail(html=None, text='Email_body', subject='Pizza online', from_email='', to_emails=[]):
