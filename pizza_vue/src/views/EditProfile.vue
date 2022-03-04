370ca75408442923b743cac60141a23dc277020c
IrePro78 <gos3r78@gmail.com>
2022-03-04 14:58:23 +0100

    add_send_mail

--- pizza_vue/src/views/EditProfile.vue
+++ pizza_vue/src/views/EditProfile.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container mt-5">
-    <div class="columns">
-      <div class="column is-4 is-offset-4">
-        <h1 class="title">Edit profile</h1>
+    <h1 class="title">Edit profile</h1>
+    <hr>
+    <div class="columns ">
+      <div class="column is-4 is-offset-1">
+
         <form @submit.prevent="changeUsername">
           <div class="field">
-            <label>Username</label>
-            <div class="control ">
+            <label>Change username</label>
+            <div class="field ">
               <p class="control has-icons-left">
                 <input type="text" name="username" class="input" v-model="username">
                 <span class="icon is-small is-left">
@@ -17,7 +19,8 @@
             <br>
             <div class="field">
               <p class="control has-icons-left">
-                <input class="input" type="***REMOVED***" placeholder="please enter your current ***REMOVED***" name="***REMOVED***" v-model="***REMOVED***">
+                <input class="input" type="***REMOVED***" placeholder="please enter your current ***REMOVED***" name="***REMOVED***"
+                       v-model="***REMOVED***">
                 <span class="icon is-small is-left">
                   <i class="fas fa-lock"></i>
                 </span>
@@ -39,8 +42,8 @@
             <label>Email</label>
             <div class="field">
               <p class="control has-icons-left">
-              <input type="email" name="email" class="input" v-model="email">
-              <span class="icon is-small is-left">
+                <input type="email" name="email" class="input" v-model="email">
+                <span class="icon is-small is-left">
                   <i class="fas fa-envelope"></i>
                </span>
               </p>
@@ -48,6 +51,41 @@
           </div>
           <div class="notification is-danger" v-if="errors2.length">
             <p v-for="error_email in errors2" v-bind:key="error_email">{{ error_email }}</p>
+          </div>
+          <div class="field">
+            <div class="control">
+              <button class="button is-success">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="column is-4 is-offset-2">
+        <form @submit.prevent="changePassword">
+          <div class="field">
+            <label>Change ***REMOVED***</label>
+            <div class="field">
+              <p class="control has-icons-left">
+                <input class="input" type="***REMOVED***" placeholder="please enter your current ***REMOVED***" name="current_***REMOVED***"
+                       v-model="current_***REMOVED***">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+            <br>
+            <div class="field">
+              <p class="control has-icons-left">
+                <input class="input" type="***REMOVED***" placeholder="please enter your new ***REMOVED***" name="new_***REMOVED***"
+                       v-model="new_***REMOVED***">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+          </div>
+          <div class="notification is-danger" v-if="errors3.length">
+            <p v-for="error_username in errors3" v-bind:key="error_username">{{ error_username }}</p>
           </div>
           <div class="field">
             <div class="control">
@@ -71,8 +109,11 @@ export default {
       username: '',
       email: '',
       ***REMOVED***: '',
+      current_***REMOVED***: '',
+      new_***REMOVED***: '',
       errors1: [],
       errors2: [],
+      errors3: [],
     }
   },
   mounted() {
@@ -144,6 +185,7 @@ export default {
                 position: 'bottom-right',
               })
               console.log(response)
+              this.***REMOVED*** = ''
               this.$store.commit('setUser', {
                 'userid': localStorage.getItem('userid'),
                 'username': this.username,
@@ -153,6 +195,7 @@ export default {
             })
             .catch(error => {
               console.log(error)
+              this.***REMOVED*** = ''
               if (error.response.status === 400) {
                 toast({
                   message: 'Please enter the correct ***REMOVED***',
@@ -163,6 +206,60 @@ export default {
                   position: 'center',
                 })
               }
+            })
+        this.$store.commit('setIsLoading', false)
+      }
+    },
+    async changePassword() {
+      this.errors3 = []
+      if (this.current_***REMOVED*** === '') {
+        this.errors3.push('The current ***REMOVED*** is missing')
+      }
+      if (this.new_***REMOVED*** === '') {
+        this.errors3.push('The new ***REMOVED*** is missing')
+      }
+      if (this.new_***REMOVED***.length < 8) {
+        this.errors3.push('The ***REMOVED*** is too short')
+      }
+      if (this.current_***REMOVED*** === this.new_***REMOVED***) {
+        this.errors3.push('The new ***REMOVED*** is the same')
+      }
+      if (!this.errors3.length) {
+        this.$store.commit('setIsLoading', true)
+        const formData = {
+          current_***REMOVED***: this.current_***REMOVED***,
+          new_***REMOVED***: this.new_***REMOVED***
+        }
+        console.log(formData)
+        await axios
+            .post('/api/v1/users/set_***REMOVED***/', formData)
+            .then(response => {
+              console.log(response.data, 'Password changed')
+
+              toast({
+                message: 'Password was changed, please log in again',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+              })
+              this.$router.push('/log-out')
+              this.$router.push('/log-in')
+            })
+            .catch(error => {
+              if (error.response.status === 400) {
+                this.***REMOVED*** = ''
+                toast({
+                  message: 'The current ***REMOVED*** is incorrect',
+                  type: 'is-danger',
+                  dismissible: true,
+                  pauseOnHover: true,
+                  duration: 2000,
+                  position: 'bottom-right',
+                })
+              }
+              console.log(error)
             })
         this.$store.commit('setIsLoading', false)
       }
