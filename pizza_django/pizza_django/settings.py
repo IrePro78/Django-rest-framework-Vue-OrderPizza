aedfcf3b8206222b273632c7e6d16c2fcd52280b
Irek <gos3r78@gmail.com>
2022-03-06 18:53:41 +0100

    add_send_mail

--- pizza_django/pizza_django/settings.py
+++ pizza_django/pizza_django/settings.py
@@ -129,7 +129,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'flaskdjangopython@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ciap3k123#'
+EMAIL_HOST_PASSWORD = ''
 
 
 # Celery Configuration Options
