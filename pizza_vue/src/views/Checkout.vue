59de1381e1810691b361117a71efaadfc6c735f4
Irek <gos3r78@gmail.com>
2022-03-06 18:56:25 +0100

    Revert "add_send_mail"
    This reverts commit 25b08017

--- pizza_vue/src/views/Checkout.vue
+++ pizza_vue/src/views/Checkout.vue
@@ -198,7 +198,7 @@ export default {
           const item = this.cart.items[i]
 
           const obj = {
-            product_variant: item.product_variant,
+            product_variant: item.product_variant.id,
             toppings: item.product_toppings,
             sauces: item.product_sauces,
             quantity: item.quantity,
